b43648f4d4b26a49be9d76e15258d1920b48cdb3
edcher <rinachernyshova@mail.ru>
2022-06-04 12:41:45 +0200

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component, VERSION } from '@angular/core';
 
+class postiTeatro {
+  nome: string;
+  valore: string; 
+  constructor(nome: string, valore: string) {
+    this.nome=nome;
+    this.valore=valore;
+  }
+}
+
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  title: string = 'Prenotazioni spettacoli';
+  posti: Array<postiTeatro> = [
+    new postiTeatro('Torino','14'),
+    new postiTeatro('Milano','15'),
+    new postiTeatro('Genova','18')
+  ];
 }
