2a5d310c43bf58e1455b07d459a5388ec506300b
edcher <rinachernyshova@mail.ru>
2022-06-04 23:19:41 +0200

    5 files modified, 4 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,10 @@
-import { Component, VERSION } from '@angular/core';
-import { PrenotazioneService } from './prenotazione.service';
-
-export class postiTeatro {
-  nome: string;
-  valore: string; 
-  constructor(nome: string, valore: string) {
-    this.nome=nome;
-    this.valore=valore;
-  }
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  title: string = 'Prenotazioni spettacoli';
-  selezione: postiTeatro;
-  posti: Array<string> = ['Torino','Milano','Genova'];
-  constructor(private ows: PrenotazioneService) { }
-  refreshTemperature(itemName: string) {
-    this.selezione = new postiTeatro(itemName, undefined);
-    this.ows.getData(this.selezione.nome).subscribe({
-      next: ( x: any ) => this.selezione.valore = x.main.temp,
-      error: err => console.error('Observer got an error: ' + err)
-    });
-
-  }
+export class AppComponent {
+  title: string = 'Prenotazioni teatro';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { PrenotazioneService } from './prenotazione.service';
 import { TeatroComponent } from './teatro/teatro.component';
+import { TeatroService } from './teatro/teatro.service';
+import { CreateatroComponent } from './createatro/createatro.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, TeatroComponent],
-  bootstrap:    [ AppComponent ],
-  providers: [ PrenotazioneService ]
+  declarations: [
+    AppComponent,
+    TeatroComponent,
+    CreateatroComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [TeatroService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
